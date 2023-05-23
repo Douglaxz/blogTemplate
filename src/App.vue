608e4f894607f7763a79196177c1b1a84fae5255
Douglas Amaral <douglaxz@hotmail.com>
2023-05-23 15:51:58 -0300

    finalizado correções do css

--- src/App.vue
+++ src/App.vue
@@ -9,22 +9,58 @@ export default {
     return {
       posts: [
         {
-          title: "meu primeiro post",
+          title: "Javascript",
+          content:
+            "JavaScript é uma linguagem de programação de alto nível, interpretada e orientada a objetos. Ela é amplamente usada no desenvolvimento web para criar interações dinâmicas e funcionais em páginas da web. JavaScript é uma linguagem versátil que permite a criação de aplicativos web, desenvolvimento de jogos, criação de aplicativos móveis e muito mais.Principais características do JavaScript:Linguagem baseada em scripts: JavaScript é executado diretamente no navegador, não requerendo um processo de compilação separado. Isso torna o desenvolvimento rápido e ágil.Integração com HTML e CSS: JavaScript pode interagir com elementos HTML e estilos CSS, permitindo manipulação e modificação dinâmica do conteúdo e do layout de uma página da web.Orientação a objetos: JavaScript suporta programação orientada a objetos, permitindo a criação de objetos, classes, herança e polimorfismo. Funcionalidades assíncronas: JavaScript possui recursos que permitem executar tarefas de forma assíncrona, como requisições HTTP, manipulação de eventos e animações, melhorando a responsividade e a experiência do usuário.Bibliotecas e frameworks populares: Existem inúmeras bibliotecas e frameworks construídos em JavaScript, como React, Angular e Vue.js, que facilitam o desenvolvimento de aplicativos web complexos. Suporte à programação do lado do cliente e do lado do servidor: JavaScript pode ser executado tanto no lado do cliente, no navegador do usuário, como no lado do servidor, usando o ambiente Node.js. Compatibilidade com múltiplos navegadores: JavaScript é suportado por todos os principais navegadores web, garantindo que os aplicativos sejam executados em diferentes plataformas e dispositivos. JavaScript desempenha um papel fundamental no desenvolvimento web moderno, permitindo a criação de páginas interativas, aplicativos web avançados e experiências de usuário envolventes. Sua popularidade e ampla adoção tornaram-no uma habilidade essencial para os desenvolvedores web.",
+          image:
+            "https://logowik.com/content/uploads/images/3799-javascript.jpg",
           datetime: new Date().toLocaleString("pt-BR"),
-          content: "meu lindo post",
+          star: 0,
+        },
+        {
+          title: "Vue.js",
+          content:
+            "Vue.js é um framework JavaScript progressivo para a construção de interfaces de usuário. Ele é amplamente utilizado no desenvolvimento web para criar aplicativos de página única (SPA) e interfaces interativas. Vue.js é conhecido por sua abordagem simples e fácil de aprender, combinando recursos de outros frameworks e bibliotecas populares. Principais características do Vue.js: Componentização: Vue.js baseia-se no conceito de componentes reutilizáveis. Os componentes encapsulam código HTML, CSS e JavaScript relacionados, permitindo a criação de interfaces modulares e de fácil manutenção. Binding bidirecional: Vue.js oferece binding de dados bidirecional entre o modelo (state) e a visualização (DOM). Isso significa que qualquer alteração no estado é refletida automaticamente na interface do usuário e vice-versa, proporcionando uma sincronização fácil e eficiente. Diretivas: O Vue.js possui diretivas que permitem estender a funcionalidade do HTML, adicionando comportamentos dinâmicos aos elementos. Diretivas populares incluem v-if, v-for e v-on, que permitem controlar renderização condicional, iteração de listas e manipulação de eventos. Reatividade: O Vue.js usa um sistema de reatividade para rastrear dependências e atualizar automaticamente a interface do usuário sempre que os dados mudam. Isso elimina a necessidade de manipulação manual do DOM, tornando o desenvolvimento mais eficiente. Ecossistema extensível: Vue.js possui uma ampla gama de bibliotecas e plugins disponíveis, permitindo a integração com ferramentas populares de desenvolvimento, como Vue Router para roteamento, Vuex para gerenciamento de estado e Vuetify para componentes de interface de usuário pré-construídos. Suporte a Single-File Components (SFC): Vue.js permite a criação de componentes em um único arquivo .vue, que encapsula código HTML, CSS e JavaScript em uma única unidade coesa. Isso facilita a organização e manutenção do código. Curva de aprendizado suave: Vue.js é conhecido por sua curva de aprendizado suave, tornando-o acessível para desenvolvedores iniciantes e experientes. Sua sintaxe intuitiva e documentação abrangente ajudam os desenvolvedores a se tornarem produtivos rapidamente. Vue.js é uma escolha popular para o desenvolvimento de aplicativos web modernos devido à sua flexibilidade, desempenho e simplicidade. Ele combina os melhores recursos de outros frameworks, fornecendo uma experiência de desenvolvimento agradável e eficiente. Com uma comunidade ativa e crescente, Vue.js continua a evoluir e se tornar uma ferramenta de destaque no desenvolvimento web.",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/555px-Vue.js_Logo_2.svg.png",
+          datetime: new Date().toLocaleString("pt-BR"),
+          star: 0,
         },
       ],
     };
   },
+  created() {
+    const postsString = localStorage.getItem("posts");
+    if (postsString) {
+      this.posts = JSON.parse(postsString);
+    }
+  },
   methods: {
     addPost(newPost) {
       this.posts.push(newPost);
+      this.savePostsToLocalStorage();
     },
-    updatePost(updatePost, id) {
-      this.posts[id] = updatePost;
+    updatePost(updatedPost, id) {
+      this.posts[id] = updatedPost;
+      this.savePostsToLocalStorage();
     },
     removePost(id) {
       this.posts.splice(id, 1);
+      this.savePostsToLocalStorage();
+    },
+    addStar(id) {
+      this.posts[id].star++;
+      this.savePostsToLocalStorage();
+    },
+    delStar(id) {
+      if (this.posts[id].star > 0) {
+        this.posts[id].star--;
+        this.savePostsToLocalStorage();
+      }
+    },
+    savePostsToLocalStorage() {
+      const postsString = JSON.stringify(this.posts);
+      localStorage.setItem("posts", postsString);
     },
   },
 };
@@ -32,15 +68,16 @@ export default {
 
 <template>
   <header>
-    <nav>
-      |&nbsp;
-      <span class="material-symbols-sharp"> home </span>
-      <RouterLink to="/">Home</RouterLink>
-      &nbsp; |&nbsp;
-      <span class="material-symbols-sharp"> add_circle </span>
-      <RouterLink to="/create"> New</RouterLink>
-      &nbsp; |
-    </nav>
+    <div class="flexCenter">
+      <RouterLink to="/"
+        ><span class="material-symbols-sharp icone"> home </span></RouterLink
+      >
+      <RouterLink to="/create">
+        <span class="material-symbols-sharp icone">
+          add_circle
+        </span></RouterLink
+      >
+    </div>
   </header>
   <main>
     <RouterView
@@ -48,6 +85,8 @@ export default {
       @create-post="addPost"
       @edit-post="updatePost"
       @delete-post="removePost"
+      @add-star="addStar"
+      @del-star="delStar"
     />
   </main>
   <footer>
--- src/components/PostLayout.vue
+++ src/components/PostLayout.vue
@@ -2,20 +2,20 @@
 export default {
   props: {
     post: Object,
-    id: String,
   },
   data() {
+    const id = this.$route.params.id;
     return {
       FormData: {
         title: this.post?.title || "",
         content: this.post?.content || "",
       },
       isEditing: Boolean(this.post),
+      id: id,
     };
   },
   methods: {
     handleClickCreatePost(event) {
-      alert(id);
       if (!this.FormData.title || !this.FormData.content) {
         alert("Por favo preencha todos os campos");
         return;
@@ -41,23 +41,49 @@ export default {
 
       this.$router.push("/");
     },
+    addStar(id) {
+      this.$emit("add-Star", id);
+    },
+    delStar(id) {
+      this.$emit("del-Star", id);
+    },
   },
 };
 </script>
 <template>
-  <form>
-    <label>Visualizar Post</label>
-  </form>
   <div id="lista-post">
     <div class="post">
-      <h3>
-        {{ post.title }}
+      <div class="flex">
+        Visualizar Post
+        <RouterLink :to="`/edit/${id}`">
+          <span class="material-symbols-sharp icone"> edit </span>
+        </RouterLink>
         <RouterLink :to="`/`">
           <span class="material-symbols-sharp icone"> reply </span>
         </RouterLink>
-      </h3>
-      <h4>{{ post.datetime }}</h4>
-      <p>{{ post.content }}</p>
+      </div>
+      <div class="divTituloData">
+        <h3>{{ post.title }}</h3>
+        &nbsp;
+        <h3>
+          {{ post.datetime }}
+        </h3>
+      </div>
+      <textarea cols="20" rows="10" class="textareaHome">{{
+        post.content
+      }}</textarea>
+      <img :src="post.image" class="imagePost" />
+      <div class="stars">
+        <span class="material-symbols-sharp iconStar" @click="delStar(id)">
+          keyboard_double_arrow_left
+        </span>
+        <span class="material-symbols-sharp iconStar"> grade </span>
+        <p class="starcount">{{ post.star }}</p>
+
+        <span class="material-symbols-sharp iconStar" @click="addStar(id)">
+          keyboard_double_arrow_right
+        </span>
+      </div>
     </div>
   </div>
 </template>
